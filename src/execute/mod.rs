0e9ed57107f7cd21c8dfa92be85b2c03da488303
Daniel Gabriele <dg0offset@gmail.com>
2023-05-29 20:09:17 -0400

    removed deprecated pay and earn funcs

--- src/execute/mod.rs
+++ src/execute/mod.rs
@@ -3,13 +3,9 @@ pub mod credit;
 pub mod pool;
 pub mod process;
 
-mod earn;
-mod pay;
 mod pay_taxes;
 mod set_config;
 
-pub use earn::earn;
-pub use pay::pay;
 pub use pay_taxes::pay_taxes;
 pub use process::process;
 pub use set_config::set_config;
